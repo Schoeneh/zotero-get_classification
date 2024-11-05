9c260a761b372db422c71000ac7114b97142c332
Schoeneh <116974162+Schoeneh@users.noreply.github.com>
2024-11-05 21:53:04 +0100

    XPATH now working

--- src/k10plus.js
+++ src/k10plus.js
@@ -1,20 +1,36 @@
 // API = https://wiki.k10plus.de/display/K10PLUS/SRU
 // Using ISBN
+// https://services.dnb.de/sru/dnb?version=1.1&operation=searchRetrieve&query=NUM%3D978-3-8353-0636-3&recordSchema=MARC21-xml
+// https://sru.kobv.de/k2?version=1.1&operation=searchRetrieve&query=dc.identifier%3D978-3-8353-0636-3&startRecord=1&maximumRecords=10&recordSchema=marcxml&recordPacking=xml
 
 // titleInfo
 
 var items = Zotero.getActiveZoteroPane().getSelectedItems();
-//const parser = new DOMParser();
-var apiCall = "https://sru.k10plus.de/opac-de-627?version=1.1&operation=searchRetrieve&query=pica.isb%3D";
-var suffix = "&maximumRecords=1&recordSchema=mods";
+var parser = new DOMParser();
+var apiCall = "https://sru.kobv.de/k2?version=1.1&operation=searchRetrieve&query=dc.identifier%3D";
+var suffix = "&startRecord=1&maximumRecords=10&recordSchema=marcxml&recordPacking=xml&stylesheet=";
 
 for (let item of items) {
+    var txt = "";
     var query = item.getField('ISBN');
     let data = await Zotero.HTTP.request("GET", apiCall + query + suffix);
     let xmlDoc = data.responseXML;
-    let classif = xmlDoc.getElementsByTagName('classification')
-    //var test = xmlDoc.getElementsByTagName('classification')[1]['attributes'][0]['value'];
-    //var test = Zotero.Utilities.xpath(xmlDoc,'.', "http://www.loc.gov/mods/v3");
-    //var s = new XMLSerializer().serializeToString(data);
-    return classif.length;
+    let test = xmlDoc.evaluate("//zs:records/zs:record/zs:recordData",xmlDoc,namespace,XPathResult.ANY_TYPE,null);
+    var result = test.iterateNext();
+    var xmlDoc2 = parser.parseFromString(result["innerHTML"],"text/html");
+    var test2 = xmlDoc2.evaluate("//datafield[@tag='084' and subfield[@code='2'] = 'rvk']/subfield[@code='a']",xmlDoc2,namespace2,XPathResult.ANY_TYPE,null);
+    var result2 = test2.iterateNext();
+    
+    //return data['response'];
+    //return xmlDoc;
+    return test;
+    //return result["innerHTML"];
+    //return result2["innerHTML"];
+}
+
+function namespace() {
+  return "http://www.loc.gov/zing/srw/";
+}
+function namespace2() {
+    return "http://www.loc.gov/MARC21/slim"
 }
