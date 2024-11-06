261a0c6261ef6ba1597fb4298f1863fd625e9ae5
Schoeneh <116974162+Schoeneh@users.noreply.github.com>
2024-11-06 10:13:45 +0100

    add items to either 'success' or 'error' collection

--- src/k10plus.js
+++ src/k10plus.js
@@ -11,33 +11,44 @@ var apiCall = "https://sru.kobv.de/k2?version=1.1&operation=searchRetrieve&query
 var suffix = "&startRecord=1&maximumRecords=10&recordSchema=marcxml&recordPacking=xml&stylesheet=";
 
 const date = new Date(Date.now())
-var collection = new Zotero.Collection();
-collection.name = "ISBN2RVK_" + date.toISOString()
-await collection.saveTx();
+var collection_error = new Zotero.Collection();
+collection_error.name = "ISBN2RVK_Error_" + date.toISOString()
+
+var collection_success = new Zotero.Collection();
+collection_success.name = "ISBN2RVK_" + date.toISOString()
 
 for (let item of items) {
     var txt = "";
     var query = item.getField('ISBN');
-    let data = await Zotero.HTTP.request("GET", apiCall + query + suffix);
-    let xmlDoc = data.responseXML;
-    let test = xmlDoc.evaluate("//zs:records/zs:record/zs:recordData",xmlDoc,namespace,XPathResult.ANY_TYPE,null);
-    
-    let node = null;
-    const RVK = [];
-    while ((node = test.iterateNext())) {
-        var xmlDoc2 = parser.parseFromString(node["innerHTML"],"text/html");
-        var test2 = xmlDoc2.evaluate("//datafield[@tag='084' and subfield[@code='2'] = 'rvk']/subfield[@code='a']",xmlDoc2,namespace2,XPathResult.ANY_TYPE,null);
+    if (!query){
+        await collection_error.saveTx();
         
-        while ((node2 = test2.iterateNext())) {
-            RVK.push(node2["innerHTML"]);
-        }
+        item.addToCollection(collection_error.key);
+        await item.saveTx();
     }
-
+    else {
+        await collection_success.saveTx();
+        
+        let data = await Zotero.HTTP.request("GET", apiCall + query + suffix);
+        let xmlDoc = data.responseXML;
+        let test = xmlDoc.evaluate("//zs:records/zs:record/zs:recordData",xmlDoc,namespace,XPathResult.ANY_TYPE,null);
     
-    item.setField('archiveLocation', RVK[0]);
-    item.addToCollection(collection.key);
-    await item.saveTx();
-    return RVK[0];
+        let node = null;
+        var RVK = [];
+        while ((node = test.iterateNext())) {
+            var xmlDoc2 = parser.parseFromString(node["innerHTML"],"text/html");
+            var test2 = xmlDoc2.evaluate("//datafield[@tag='084' and subfield[@code='2'] = 'rvk']/subfield[@code='a']",xmlDoc2,namespace2,XPathResult.ANY_TYPE,null);
+        
+            while ((node2 = test2.iterateNext())) {
+                RVK.push(node2["innerHTML"]);
+            }
+        }
+
+        item.setField('archiveLocation', RVK[0]);
+        item.addToCollection(collection_success.key);
+        await item.saveTx();
+        return RVK[0];
+    }
 }
 
 function namespace() {
